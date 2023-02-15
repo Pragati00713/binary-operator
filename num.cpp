99af0a5942ff124ec5d75357f231ebf11b26c540
Pragati00713 <kushwahapragati713@gmail.com>
2023-02-14 18:43:26 -0800

    comparison

---
+++ num.cpp
@@ -0,0 +1,13 @@
+# include<iostream>
+using namespace std;
+
+int main(){
+    int a = 20, b= 7, c=20;
+    cout<<(a>b)<<endl;
+    cout<<(a<b)<<endl;
+    cout<<(a<=b)<<endl;
+    cout<<(a==b)<<endl;
+    cout<<(a==b)<<endl;
+    cout<<(a!=c)<<endl;
+
+}
